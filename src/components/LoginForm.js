5ef5dde9d219e99b847cb3a1cd3b8992561962bd
myelsoria <myrell.05@gmail.com>
2021-04-02 22:31:16 +0800

    login form fields validation

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Form, Button, Alert } from 'react-bootstrap'
 import { useFormik } from 'formik'
+import { loginFormValidation } from '../validation/loginFormValidation'
 
 function LoginForm() {
   const [loginError, setLoginError] = useState('')
@@ -10,6 +11,7 @@ function LoginForm() {
       username: '',
       password: '',
     },
+    validationSchema: loginFormValidation,
     onSubmit: (values) => {
       setLoginError('')
       if (values.username !== 'myaccount' || values.password !== '123456')
@@ -24,6 +26,9 @@ function LoginForm() {
           placeholder='username'
           {...formik.getFieldProps('username')}
         />
+        {formik.touched.username && formik.errors.username ? (
+          <span className='text-danger'>{formik.errors.username}</span>
+        ): null }
       </Form.Group>
       <Form.Group controlId='password'>
         <Form.Control
@@ -31,6 +36,9 @@ function LoginForm() {
           placeholder='password'
           {...formik.getFieldProps('password')}
         />
+        {formik.touched.password && formik.errors.password ? (
+          <span className='text-danger'>{formik.errors.password}</span>
+        ) : null }
       </Form.Group>
       <Form.Group controlId='loginBtn'>
         <Button type='submit' className='btn-block' variant='primary'>
