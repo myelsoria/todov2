711a1d77b18c3e2f175253f89797544b767980bd
myelsoria <myrell.05@gmail.com>
2021-04-02 23:11:54 +0800

    enabled routing for pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
+import TodoListPage from './pages/TodoListPage'
+
 function App() {
   return (
     <div className="App min-vh-100">
-      <LoginPage />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={LoginPage}/>
+          <Route path='/todolist' component={TodoListPage}/>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
